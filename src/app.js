001c64e222664fa2b34fe4c81123ef658d8d7da1
Anoop Santhanam <anoop.santhanam@gmail.com>
2024-01-20 12:26:28 +0100

    Added route to display unpaid jobs for client or contractor

--- src/app.js
+++ src/app.js
@@ -32,4 +32,37 @@ app.get("/profiles", async (req, res) => {
   res.status(200).json(profiles);
 });
 
+/**
+ * Gets unpaid jobs for client or contractor
+ *
+ * @returns {Job[]} jobs
+ */
+app.get("/jobs/unpaid", getProfile, async (req, res) => {
+  const { Job, Contract } = req.app.get("models");
+
+  const clause = req.profile.type === "client" ? "ClientId" : "ContractorId";
+
+  const contracts = await Contract.findAll({
+    where: {
+      status: "in_progress",
+      [clause]: req.profile.id,
+    },
+  });
+
+  const unpaidJobsPromises = contracts.map(async (contract) => {
+    const unpaidJob = await Job.findAll({
+      where: {
+        ContractId: contract.id,
+        paid: false || null,
+      },
+    });
+
+    return unpaidJob;
+  });
+
+  const unpaidJobsList = await Promise.all(unpaidJobsPromises);
+
+  return res.status(200).json(unpaidJobsList.flat());
+});
+
 module.exports = app;
